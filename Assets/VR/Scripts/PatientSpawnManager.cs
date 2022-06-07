3d1165e8e486f46e095ccf9b619863c13b5f5a78
ireita <59600843+ireita@users.noreply.github.com>
2022-06-07 23:40:40 +0200

    Basic Patient Behaviour Tree, Stopwatch, Spawner logic
    Basic Patient Behaviour Tree, Stopwatch, Spawner logic

---
+++ Assets/VR/Scripts/PatientSpawnManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientSpawnManager : MonoBehaviour
+{
+
+    private int spawninterval;
+    private float time; //used to track time 
+    private int seconds; //used to check when spawninterval is equal
+    [SerializeField] private GameObject Patient;
+    [SerializeField] private Stopwatch Stopwatch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawninterval = Random.Range(4, 7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        seconds = (int) (time % 60);
+        if(spawninterval == seconds)
+        {
+            HandleSpawn();
+        }
+    }
+
+    private void HandleSpawn()
+    {
+        time = 0;
+        //spawninterval = Random.Range(10, 20);
+        Patient.SetActive(true);
+        Stopwatch.StartStopWatch();
+
+    }
+}
---
+++ Assets/VR/Scripts/Stopwatch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Stopwatch : MonoBehaviour
+{
+
+    public bool stopwatchActive;
+    private float currentTime;
+    [SerializeField] private TextMeshProUGUI currentTimeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stopwatchActive)
+        {
+            currentTime += Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = "Aktueller Patient: " + time.ToString(@"mm\:ss\:fff");
+    }
+
+    public void StartStopWatch()
+    {
+        stopwatchActive = true;
+    }
+
+    public void StopStopWatch()
+    {
+        currentTime = 0;
+        stopwatchActive = false;
+    }
+}
